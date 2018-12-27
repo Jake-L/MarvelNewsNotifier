2d8d16e55b9864384b8c489a267ae3e6b7fd17d8
Jake <jake.loftus@outlook.com>
2018-12-27 15:51:26 -0500

    change string decoding and create ssl context

--- hachette.py
+++ hachette.py
@@ -4,6 +4,10 @@ from datetime import datetime
 from datetime import timedelta
 import smtplib
 from urllib.request import urlopen
+import ssl
+
+# create ssl settings for reading the url
+ssl._create_default_https_context = ssl._create_unverified_context
 
 email_addr = []
 src_email = "marvelcollectionsnews@gmail.com"
@@ -12,6 +16,7 @@ email_list = []
 first_run = True
 INTERVAL = 900
 
+# read the password from a text file
 with open("password.txt", 'r') as f:
     PASSWORD = f.read().strip()
 
@@ -21,6 +26,7 @@ def sleep_before_checking():
 
 while True:
     email_list = []
+    # try to open the url, and restart the loop if it fails
     try:
         response = urlopen("https://www.hachettebookgroup.biz/search/?q=marvel&sort_by=-pub_date&results_per_page=100")
     except:
@@ -28,8 +34,11 @@ while True:
         sleep_before_checking()
         continue
     line_counter = -1
+
+    # read every line from the webpage
     for rawline in response:
-        line = str( rawline, encoding='utf8' )
+        line = rawline.decode('utf-8')
+        # this string appears 3 lines before the line we want
         if '<ul class="results-gallery">' in line:
             line_counter = 3
 
@@ -39,8 +48,9 @@ while True:
             if title not in title_list:
                 title_list.append(title)
 
+                # don't send emails on the first run
                 if first_run == False:
-                    email_list.append(title.replace('&amp;','&'))
+                    email_list.append(title.replace('&amp;','&').replace('&#39;',"'"))
 
         if line_counter >= 0:
             line_counter -= 1
@@ -48,14 +58,17 @@ while True:
 
     # send the email with the new titles
     if len(email_list) > 0:
-        message = 'Subject: {} new Marvel collections posted on hachette\n\n{}\n\nIf you wish to stop receiving these emails, reply "UNSUBSCRIBE"'.format(len(email_list), "\n".join(email_list))
-
+        message = """Subject: {} new Marvel collections posted on hachette
+{}
+\nIf you wish to stop receiving these emails, reply "UNSUBSCRIBE"
+""".format(len(email_list), "\n".join(email_list))
+        
         # read the list of email addresses from a text file
         with open("emails.txt", 'r') as f:
             email_addr = f.read().splitlines()
             
         print("send email for {} new titles to {} users".format(len(email_list), len(email_addr)))
-
+        
         # send the email
         try:
             with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
